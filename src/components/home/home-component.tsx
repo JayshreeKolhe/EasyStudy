4131e527f08fddbc8df74c999ba5788c8458ef87
JayshreeKolhe <jayshreekolhe0@gmail.com>
2020-03-09 22:13:10 +0100

    component file

---
+++ src/components/home/home-component.tsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+import './home.scss';
+
+export default class HomeComponent extends Component {
+    render() {
+      return (
+          <div>
+              <h5>Home</h5>
+          </div>
+      )
+    }
+}
+  
