d04ba549e22c4bce2c3f494ee49825582ea8e33a
JayshreeKolhe <jayshreekolhe0@gmail.com>
2020-03-10 22:55:39 +0100

    Header, footer and basic home page added

--- src/components/home/home-component.tsx
+++ src/components/home/home-component.tsx
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import './home.scss';
+import Typography from '@material-ui/core/Typography';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Container from '@material-ui/core/Container';
+import homepage from '../../assets/images/homepage-image.png'
 
 export default class HomeComponent extends Component {
-    render() {
-      return (
-          <div>
-              <h5>Home</h5>
-          </div>
-      )
-    }
-}
   
+  render(){
+  return (
+    <React.Fragment>
+      <CssBaseline />
+      <Container fixed>
+        <Typography component="div" style={{ backgroundColor: '#FC5252', height: '911pt' }} >
+
+        <img src = {homepage} style={{height:'400pt',width:'400pt'}}>
+
+        </img>
+        </Typography>>
+      </Container>
+    </React.Fragment>
+  );
+}
+}
